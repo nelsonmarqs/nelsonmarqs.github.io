eae28fd45934cba35a680b851c346126660f5296
nelsonmarqs <120415922+nelsonmarqs@users.noreply.github.com>
2024-07-31 11:08:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Pre Loading
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(10).fadeOut('slow'); 
+    $('body').delay(10).css({'overflow': 'visible'});
+});
+
+// Carregar HTML externo
+$(document).ready(function () {
+    $("#menu").load("html/principal/menu.html");
+    $("#sub-roda-pe").load("html/principal/sub-roda-pe.html");
+    $("#roda-pe").load("html/principal/roda-pe.html");
+    $("#stories").load("html/stories/stories.html");
+    $("#post").load("html/post/post.html");
+});
+
+// Index Newsletter Scroll
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.hash === '#newsletter') {
+        var scrollPosition = 890;
+
+        window.scrollTo({
+            top: scrollPosition,
+            behavior: 'smooth'  // Scroll suave
+        });
+    }
+});
