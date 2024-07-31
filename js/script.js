c790ec529b10010e4090b5404c8aa6cf1f7e7d85
nelsonmarqs <120415922+nelsonmarqs@users.noreply.github.com>
2024-07-31 13:33:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Pre Loading
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(10).fadeOut('slow'); 
+    $('body').delay(10).css({'overflow': 'visible'});
+});
+
 // Carregar HTML externo
 $(document).ready(function () {
     $("#menu").load("html/principal/menu.html");
